fb913b19411614807d765349a8e35705a6b3fdbd
francostellari <fstellari@gmail.com>
2023-09-28 13:02:24 -0400

    mc

--- mc/helm-chart/dry-run.sh
+++ mc/helm-chart/dry-run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-helm install kubestellar . \
-   --set global.EXTERNAL_HOSTNAME=kcp.apps.edgeplatform1-9ca4d14d48413d18ce61b80811ba4308-0000.us-south.containers.appdomain.cloud  \
+ helm install kubestellar . \
+    --set kcp.externalHostname=kcp.apps.edgeplatform1-9ca4d14d48413d18ce61b80811ba4308-0000.us-south.containers.appdomain.cloud \
+    --set kcp.kcp.volumeClassName=ibmc-block-gold \
    --dry-run --debug  > /vagrant/kubestellar.yaml
