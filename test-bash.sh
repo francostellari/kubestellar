243a0e69983c2902f4be0f93e88438b4f379adbe
francostellari <fstellari@gmail.com>
2025-02-28 11:56:56 -0500

    test bash
    Signed-off-by: francostellari <fstellari@gmail.com>

--- test-bash.sh
+++ test-bash.sh
@@ -4,6 +4,9 @@ echo "script=$0"
 echo "N=$#"
 echo "args=\"$@\""
 
+[[ "$0" == *bash ]] && echo "curl|bash"
+echo "last=\"$(history | tail -n 1)\""
+
 i=0
 for arg in "$@"; do
     echo "arg[$1]=$arg"
